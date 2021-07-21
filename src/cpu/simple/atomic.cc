f776299adde8889e8f10d32176796b95dd867fff
Gabe Black <gabe.black@gmail.com>
2021-07-21 04:15:24 -0700

    arch,cpu,mem,sim: Fold arch/locked_mem.hh into the BaseISA class.
    Turn the functions within it into virtual methods on the ISA classes.
    Eliminate the implementation in MIPS, which was just copy pasted from
    Alpha long ago. Fix some minor style issues in ARM. Remove templating.
    Switch from using an "XC" type parameter to using the ThreadContext *
    installed in all ISA classes.
    
    Jira Issue: https://gem5.atlassian.net/browse/GEM5-1053
    
    Change-Id: I19ee3a8fbe50a4d7907029c2dd2796d0e98e965f
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/48384
    Reviewed-by: Gabe Black <gabe.black@gmail.com>
    Reviewed-by: Giacomo Travaglini <giacomo.travaglini@arm.com>
    Maintainer: Gabe Black <gabe.black@gmail.com>
    Tested-by: kokoro <noreply+kokoro@google.com>

--- src/cpu/simple/atomic.cc
+++ src/cpu/simple/atomic.cc
@@ -41,7 +41,6 @@
 
 #include "cpu/simple/atomic.hh"
 
-#include "arch/locked_mem.hh"
 #include "base/output.hh"
 #include "config/the_isa.hh"
 #include "cpu/exetrace.hh"
@@ -134,8 +133,8 @@ AtomicSimpleCPU::threadSnoop(PacketPtr pkt, ThreadID sender)
                 wakeup(tid);
             }
 
-            TheISA::handleLockedSnoop(threadInfo[tid]->thread,
-                                      pkt, dcachePort.cacheBlockMask);
+            threadInfo[tid]->thread->getIsaPtr()->handleLockedSnoop(pkt,
+                    dcachePort.cacheBlockMask);
         }
     }
 }
@@ -299,7 +298,8 @@ AtomicSimpleCPU::AtomicCPUDPort::recvAtomicSnoop(PacketPtr pkt)
         DPRINTF(SimpleCPU, "received invalidation for addr:%#x\n",
                 pkt->getAddr());
         for (auto &t_info : cpu->threadInfo) {
-            TheISA::handleLockedSnoop(t_info->thread, pkt, cacheBlockMask);
+            t_info->thread->getIsaPtr()->handleLockedSnoop(pkt,
+                    cacheBlockMask);
         }
     }
 
@@ -325,7 +325,8 @@ AtomicSimpleCPU::AtomicCPUDPort::recvFunctionalSnoop(PacketPtr pkt)
         DPRINTF(SimpleCPU, "received invalidation for addr:%#x\n",
                 pkt->getAddr());
         for (auto &t_info : cpu->threadInfo) {
-            TheISA::handleLockedSnoop(t_info->thread, pkt, cacheBlockMask);
+            t_info->thread->getIsaPtr()->handleLockedSnoop(pkt,
+                    cacheBlockMask);
         }
     }
 }
@@ -408,7 +409,7 @@ AtomicSimpleCPU::readMem(Addr addr, uint8_t *data, unsigned size,
             assert(!pkt.isError());
 
             if (req->isLLSC()) {
-                TheISA::handleLockedRead(thread, req);
+                thread->getIsaPtr()->handleLockedRead(req);
             }
         }
 
@@ -483,9 +484,8 @@ AtomicSimpleCPU::writeMem(uint8_t *data, unsigned size, Addr addr,
 
             if (req->isLLSC()) {
                 assert(curr_frag_id == 0);
-                do_access =
-                    TheISA::handleLockedWrite(thread, req,
-                                              dcachePort.cacheBlockMask);
+                do_access = thread->getIsaPtr()->handleLockedWrite(req,
+                        dcachePort.cacheBlockMask);
             } else if (req->isSwap()) {
                 assert(curr_frag_id == 0);
                 if (req->isCondSwap()) {
